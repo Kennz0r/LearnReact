ece80f6e7a8b285720d1512156f44de4e2a1b126
Kennz0r <37302430+Kennz0r@users.noreply.github.com>
2020-04-01 23:39:40 +0200

    tutorial done

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ class App extends Component {
     ]
   };
 
+  constructor() {
+    super();
+  }
+
   handleIncrement = counter => {
     const counters = [...this.state.counters];
     const index = counters.indexOf(counter);
